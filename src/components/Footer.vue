f4a6eb82bb0e6c81592ff28dbf530963ddf6bca8
maulanairfanf <maulanairfanf@gmail.com>
2024-04-03 13:48:54 +0700

    create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer class="text-center d-flex flex-column">
+    <div>
+      {{ new Date().getFullYear() }} — <strong>Newtronic Solution</strong>
+    </div>
+  </v-footer>
+</template>
