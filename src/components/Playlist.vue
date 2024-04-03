a1c1b342275dc9eac3bc8f24f04a6560cffd4195
maulanairfanf <maulanairfanf@gmail.com>
2024-04-03 13:48:29 +0700

    create playlist

---
+++ src/components/Playlist.vue
@@ -0,0 +1,21 @@
+<script setup>
+import PlaylistItems from "./PlaylistItems.vue";
+
+defineProps({
+  listPlaylist: {
+    type: Array,
+    default: () => [],
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <v-container>
+    <div class="d-flex flex-wrap justify-space-around">
+      <div v-for="(item, index) in listPlaylist" :key="index">
+        <PlaylistItems :item="item" />
+      </div>
+    </div>
+  </v-container>
+</template>
---
+++ src/components/PlaylistItems.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  item: {
+    type: Object,
+    default: () => {},
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <v-card class="mx-auto my-12" max-width="350">
+    <v-img
+      height="250"
+      :src="item.url"
+      cover
+      v-if="item.type === 'image'"
+    ></v-img>
+    <!-- :src="item.url" saya belum dapat menampilkan video yang ada pada API -->
+    <iframe
+      v-else-if="item.type === 'video'"
+      height="250"
+      width="100%"
+      src="https://www.youtube.com/embed/ePmYL1L3psg"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+
+    <v-card-item>
+      <v-card-title>{{ item.title }}</v-card-title>
+
+      <div>
+        <span>{{ item.description }}</span>
+      </div>
+    </v-card-item>
+  </v-card>
+</template>
