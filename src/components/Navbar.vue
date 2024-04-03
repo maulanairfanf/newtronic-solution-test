4272a317e05125d5a1faf51c599031978fffdef4
maulanairfanf <maulanairfanf@gmail.com>
2024-04-03 13:48:41 +0700

    create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  logo: {
+    type: String,
+    default: "",
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <v-container>
+    <v-app-bar app>
+      <v-toolbar-title>
+        <v-img :src="logo" width="250"></v-img>
+      </v-toolbar-title>
+    </v-app-bar>
+  </v-container>
+</template>
