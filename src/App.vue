e90efbd3d29f435528cd45832d54c1c4db15350b
maulanairfanf <maulanairfanf@gmail.com>
2024-04-03 13:49:13 +0700

    integrate api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,64 @@
+<script setup>
+import axios from "axios";
+import { onMounted, ref, reactive } from "vue";
+import Hero from "./components/Hero.vue";
+import Playlist from "./components/Playlist.vue";
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+const logo = ref("");
+const listPlaylist = ref([]);
+const isLoading = ref(true);
+
+const heroSection = reactive({
+  title: "",
+  description: "",
+  banner: "",
+});
+
+async function fetchApi() {
+  isLoading.value = true;
+  try {
+    const response = await axios.get(
+      "http://103.183.75.112/api/directory/dataList"
+    );
+    if (response) {
+      handleData(response.data.data[0]);
+      console.log("response: ", response.data.data[0]);
+    }
+  } catch (error) {
+    console.log("error: ", error);
+  }
+  isLoading.value = false;
+}
+
+function handleData(payload) {
+  logo.value = payload.logo;
+  heroSection.title = payload.title;
+  heroSection.description = payload.description;
+  heroSection.banner = payload.banner;
+  listPlaylist.value = payload.playlist;
+}
+
+onMounted(async () => {
+  await fetchApi();
+});
+</script>
+
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
-
-    <AppFooter />
+    <div v-if="isLoading">IsLoading...</div>
+    <div v-else>
+      <Navbar :logo="logo" />
+      <v-main>
+        <Hero
+          :title="heroSection.title"
+          :description="heroSection.description"
+          :banner="heroSection.banner"
+        />
+        <Playlist :listPlaylist="listPlaylist" />
+      </v-main>
+      <Footer />
+    </div>
   </v-app>
 </template>
-
-<script setup>
-  //
-</script>
